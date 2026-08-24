f25f55a48dbe8cb442df5ee691957933f242d8aa
David Craig Cothren <dacracot@yahoo.com>
2026-08-24 15:14:30 -0700

    click for shapes

--- smash.js
+++ smash.js
@@ -1,3 +1,8 @@
+const CENTERHIGH = 400;
+const CENTERLOW = 200;
+const DEGREESHIGH = 360;
+const DEGREESLOW = 0;
+
 function starPoints(cx, cy, outerRadius, innerRadius, points = 5, rotation = -Math.PI / 2) {
 	const coordinates = [];
 	for (let i = 0; i < points * 2; i++) {
@@ -12,26 +17,60 @@ function randomIntegerRange (min = 0, max = 1200) {
 	return(Math.round((Math.random()*(max-min))+min));
 	}
 
-const COLORS = ["red","orange","yellow","green","blue","indego","violet"];
+const COLORS = ["red","orange","yellow","green","blue","indigo","violet"];
+const COLORSMAX = 6;
+const COLORSMIN = 0;
 
 function randomColor () {
-	return(COLORS[randomIntegerRange(0,6)]);
+	return(COLORS[randomIntegerRange(COLORSMIN,COLORSMAX)]);
 	}
+
+const SCREEN = document.getElementById("screen");
+const SHAPESMAX = 2;
+const SHAPESMIN = 0;
+const RADIUSHIGH = 180;
+const RADIUSLOW = 16;
 
 function boop () {
-	shape =
-		"<polygon points=\"" +
-		starPoints(randomIntegerRange(0,100), randomIntegerRange(0,100), randomIntegerRange(60,120), randomIntegerRange(10,60), randomIntegerRange(2,24)) +
-		"\" fill=\"" +
-		randomColor() +
-		"\" transform=\"rotate(" +
-		randomIntegerRange(0,100)+
-		") translate(" +
-		randomIntegerRange(0,100)+
-		"," +
-		randomIntegerRange(0,100)+
-		")\" />";
-	return(shape);
+	shape = "";
+	switch(randomIntegerRange(SHAPESMIN,SHAPESMAX)) {
+		case 0:
+			shape +=
+				"<circle cx=\"" +
+				randomIntegerRange(CENTERLOW,CENTERHIGH) +
+				"\" cy=\"" +
+				randomIntegerRange(CENTERLOW,CENTERHIGH) +
+				"\" r=\"" +
+				randomIntegerRange(RADIUSLOW,RADIUSHIGH) +
+				"\" fill=\"" +
+				randomColor() +
+				"\" />";
+			break;
+		case 1:
+			shape +=
+				"<rect x=\"" +
+				randomIntegerRange(CENTERLOW,CENTERHIGH)+
+				"\" y=\"" +
+				randomIntegerRange(CENTERLOW,CENTERHIGH)+
+				"\" width=\"" +
+				randomIntegerRange(RADIUSLOW,RADIUSHIGH)+
+				"\" height=\"" +
+				randomIntegerRange(RADIUSLOW,RADIUSHIGH)+
+				"\" fill=\"" +
+				randomColor() +
+				"\" transform=\"rotate(" +
+				randomIntegerRange(DEGREESLOW,DEGREESHIGH)+
+				")\" />";
+			break;
+		default:
+			shape +=
+				"<polygon points=\"" +
+				starPoints(randomIntegerRange(CENTERLOW,CENTERHIGH), randomIntegerRange(CENTERLOW,CENTERHIGH), randomIntegerRange(60,120), randomIntegerRange(10,60), randomIntegerRange(2,24)) +
+				"\" fill=\"" +
+				randomColor() +
+				"\" transform=\"rotate(" +
+				randomIntegerRange(DEGREESLOW,DEGREESHIGH)+
+				")\" />";
+		}
+	SCREEN.innerHTML +=shape;
 	}
-
-document.getElementById("screen").innerHTML = boop();
