f913c60e762494078d32c232d054f819280b1154
David Craig Cothren <dacracot@yahoo.com>
2026-08-24 08:50:33 -0700

    generated shape

--- smash.js
+++ smash.js
@@ -1,27 +1,37 @@
-// code
-function starPolygon(cx, cy, outerRadius, innerRadius, points = 5,
-                     rotation = -Math.PI / 2) {
+function starPoints(cx, cy, outerRadius, innerRadius, points = 5, rotation = -Math.PI / 2) {
+	const coordinates = [];
+	for (let i = 0; i < points * 2; i++) {
+		const radius = i % 2 === 0 ? outerRadius : innerRadius;
+		const angle = rotation + i * Math.PI / points;
+        coordinates.push(`${cx + radius * Math.cos(angle)},${cy + radius * Math.sin(angle)}`);
+		}
+	return(coordinates.join(" "));
+	}
 
-    const coordinates = [];
+function randomIntegerRange (min = 0, max = 1200) {
+	return(Math.round((Math.random()*(max-min))+min));
+	}
 
-    for (let i = 0; i < points * 2; i++) {
-        const radius = i % 2 === 0 ? outerRadius : innerRadius;
-        const angle = rotation + i * Math.PI / points;
+const COLORS = ["red","orange","yellow","green","blue","indego","violet"];
 
-        coordinates.push(
-            `${cx + radius * Math.cos(angle)},${cy + radius * Math.sin(angle)}`
-        );
-    }
+function randomColor () {
+	return(COLORS[randomIntegerRange(0,6)]);
+	}
 
-    return coordinates.join(" ");
-}
+function boop () {
+	shape =
+		"<polygon points=\"" +
+		starPoints(randomIntegerRange(0,100), randomIntegerRange(0,100), randomIntegerRange(60,120), randomIntegerRange(10,60), randomIntegerRange(2,24)) +
+		"\" fill=\"" +
+		randomColor() +
+		"\" transform=\"rotate(" +
+		randomIntegerRange(0,100)+
+		") translate(" +
+		randomIntegerRange(0,100)+
+		"," +
+		randomIntegerRange(0,100)+
+		")\" />";
+	return(shape);
+	}
 
-// const points = starPolygon(100, 100, 90, 40);
-// document.querySelector("polygon").setAttribute("points", points);
-
-console.log(starPolygon(100, 100, 90, 40, 2));  // diamond
-console.log(starPolygon(100, 100, 90, 40, 3));  // 3-point star
-console.log(starPolygon(100, 100, 90, 40, 4));  // 4-point star
-console.log(starPolygon(100, 100, 90, 40, 5));  // 5-point star
-console.log(starPolygon(100, 100, 90, 40, 24));
-
+document.getElementById("screen").innerHTML = boop();
