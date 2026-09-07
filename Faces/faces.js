ff088ce9d4f26b5cc3235e8ef27a868c5d6175fb
David Craig Cothren <dacracot@yahoo.com>
2026-09-06 21:27:38 -0700

    work in progress to enable face edit, local store

--- Faces/faces.js
+++ Faces/faces.js
@@ -1,15 +1,13 @@
-const FACES = {
-	"faces": [
-		{"file": "Harold.png", "name": "Daddy"},
-		{"file": "Karen.png", "name": "Mommy"},
-		{"file": "Harry.png", "name": "Harry"},
-		{"file": "Cindy.png", "name": "Cindy"},
-		{"file": "Dog.png", "name": "Fido"}
-		]
-	}
 // --------------------------------------------------------------------
-<input type="file" id="fileInput">
-//---------------------------------------------------------------------
+// const FACES = {
+// 	"faces": [
+// 		{"file": "Harold.png", "name": "Daddy"},
+// 		{"file": "Karen.png", "name": "Mommy"},
+// 		{"file": "Harry.png", "name": "Harry"},
+// 		{"file": "Cindy.png", "name": "Cindy"},
+// 		{"file": "Dog.png", "name": "Fido"}
+// 		]
+// 	}
 //---------------------------------------------------------------------
 const DATABASE = "BabySmashDB";
 const VERSION = 1;
@@ -17,6 +15,10 @@ const STORE = "faces";
 //---------------------------------------------------------------------
 function openDatabase() {
 	return new Promise((resolve, reject) => {
+		if (database) {
+			resolve(database);
+			return;
+			}
 		const request = indexedDB.open(DATABASE, VERSION);
 		request.onupgradeneeded = event => {
 			const db = event.target.result;
@@ -32,10 +34,11 @@ function openDatabase() {
 		});
 	}
 //---------------------------------------------------------------------
+const database = await openDatabase();
+//---------------------------------------------------------------------
 async function storeFile(faceName,faceImageFile) {
-	const db = await openDatabase();
 	return new Promise((resolve, reject) => {
-		const transaction = db.transaction(STORE, "readwrite");
+		const transaction = database.transaction(STORE, "readwrite");
 		const store = transaction.objectStore(STORE);
 		const record = {
 			name: faceName,
@@ -48,15 +51,29 @@ async function storeFile(faceName,faceImageFile) {
 	}
 //---------------------------------------------------------------------
 async function getFile(id) {
-	const db = await openDatabase();
 	return new Promise((resolve, reject) => {
-		const transaction = db.transaction(STORE, "readonly");
+		const transaction = database.transaction(STORE, "readonly");
 		const store = transaction.objectStore(STORE);
 		const request = store.get(id);
 		request.onsuccess = () => resolve(request.result);
 		request.onerror = () => reject(request.error);
 		});
 	}
+//---------------------------------------------------------------------
+async function getFileCount() {
+	return new Promise((resolve, reject) => {
+		const transaction = database.transaction(STORE, "readonly");
+		const store = transaction.objectStore(STORE);
+		const request = store.count();
+		request.onsuccess = () => resolve(request.result);
+		request.onerror = () => reject(request.error);
+		});
+	}
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+<input type="file" id="fileInput">
 //---------------------------------------------------------------------
 // Handle the file upload
 document.getElementById("fileInput").addEventListener("change", async event => {
@@ -74,6 +91,11 @@ document.getElementById("fileInput").addEventListener("change", async event => {
 	});
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
-const record = await getFile(1);
-console.log(record.name);
-console.log(URL.createObjectURL(record.file));
+
+const FACESMAX = getFileCount();
+for (let i = 0; i < FACESMAX; i++) {
+	const record = await getFile(i);
+	console.log(record.name);
+	console.log(URL.createObjectURL(record.file));
+	}
+
