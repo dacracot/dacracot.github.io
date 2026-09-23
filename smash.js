4b3ae4c2980ce603bc492e1eba521426ed1cc9ea
David Craig Cothren <dacracot@yahoo.com>
2026-09-23 09:30:13 -0700

    working, but 2 clicks to set mode?

--- smash.js
+++ smash.js
@@ -210,38 +210,43 @@ function digits () {
 	}
 // --------------------------------------------------------------------
 function boop () {
-//			console.log("--1--");
+console.log("-1- MODE = "+MODE);
 	DIALOG.close();
-//			console.log("--2--");
+console.log("-2-");
 	switch(MODE) {
 		case 0:
-//			console.log("--3--");
+console.log("-3-");
 			shapes();
-//			console.log("--4--");
+console.log("-4-");
 			break;
 		case 1:
-//			console.log("--5--");
+console.log("-5-");
 			digits();
-//			console.log("--6--");
+console.log("-6-");
 			break;
 		case 2:
-//			console.log("--7--");
+console.log("-7-");
 			letters();
-//			console.log("--8--");
+console.log("-8-");
 			break;
 		default:
-//			console.log("--9--");
+console.log("-9-");
 			break;
 		}
-//			console.log("--10--");
+console.log("-10-");
 	return false;
 	}
 //---------------------------------------------------------------------
-document.addEventListener("click", function(event){
-//			console.log("-1-");
-	boop();
-//			console.log("-2-");
-	});
+function selected(mode) {
+	MODE = mode;
+	document.addEventListener("mousedown", function(event){
+console.log(event);
+console.log("--1--");
+		boop();
+console.log("--2--");
+		});
+	}
+//---------------------------------------------------------------------
 //---------------------------------------------------------------------
 DIALOG.show();
 //---------------------------------------------------------------------
