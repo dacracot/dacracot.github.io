57b1243c7c530b9436b811204880652721664f91
David Craig Cothren <dacracot@yahoo.com>
2026-09-23 20:12:43 -0700

    moved sound to shapes to make calls in animals more uniform

--- Shapes/shapes.js
+++ Shapes/shapes.js
@@ -1,2 +1,15 @@
 const SHAPES = {
-	}
+	};
+// ------------------------------
+const SOUNDS = [
+	"Shapes/Sounds/boing.mp3",
+	"Shapes/Sounds/blip.mp3",
+	"Shapes/Sounds/toot.mp3",
+	"Shapes/Sounds/blurp.mp3",
+	"Shapes/Sounds/floop.mp3",
+	"Shapes/Sounds/pluck.mp3",
+	"Shapes/Sounds/honk.mp3",
+	"Shapes/Sounds/peop.mp3",
+	"Shapes/Sounds/bell.mp3",
+	"Shapes/Sounds/crack.mp3"
+	];
